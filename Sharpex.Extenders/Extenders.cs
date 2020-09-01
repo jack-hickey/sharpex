bb431d260728114d91139f3d65a65f2d18f3a602
Jack Hickey <jack.hickey@outlook.com>
2020-09-01 18:47:56 +0100

    Added extenders for converting between object instances and XML

--- Sharpex.Extenders/Extenders.cs
+++ Sharpex.Extenders/Extenders.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
@@ -8,11 +9,34 @@ using System.Security;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Sharpex.Extenders
 {
     public static class Extenders
     {
+        public static T ToInstance<T>(this string xml)
+        {
+            using (StringReader reader = new StringReader(xml))
+            {
+                Type tarType = typeof(T);
+
+                return (T)new XmlSerializer(tarType).
+                    Deserialize(reader);
+            }
+        }
+
+        public static string ToXML(this object instance)
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                new XmlSerializer(instance.GetType()).
+                    Serialize(writer, instance);
+
+                return writer.ToString();
+            }
+        }
+
         public static string ToRealString(this SecureString value)
         {
             IntPtr valuePtr = IntPtr.Zero;
