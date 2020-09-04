bdbdba249c2c0d8167fe40727fe1346a98691c8b
Jack Hickey <jack.hickey@outlook.com>
2020-09-04 20:54:10 +0100

    Added WPF namespace with an extender for removing a databound object

---
+++ Sharpex.Extenders/WPF/WPFExtenders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sharpex.Extenders.WPF
+{
+    public static class WPFExtenders
+    {
+        /// <summary>
+        /// Remove an object from the datagrid, even when bound by an itemssource
+        /// </summary>
+        /// <param name="grid">The grid to remove the object from</param>
+        /// <param name="data">The object to remove from the grid</param>
+        public static void RemoveDataBoundObject(this DataGrid grid, object data)
+        {
+            ObservableCollection<object> obsCollection = new ObservableCollection<object>(grid.ItemsSource as IEnumerable<object>);
+
+            obsCollection.Remove(data);
+            grid.ItemsSource = obsCollection;
+        }
+    }
+}
